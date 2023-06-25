5146e75c05f9995e07edbece8615ee7e55f675fc
Phentos <phentos@gmail.com>
2023-06-25 07:35:10 -0700

    rf toucheventhandler iteration

--- touch.js
+++ touch.js
@@ -1,3 +1,10 @@
+const touchEventHandlers = [
+	["touchstart", handleStart],
+	["touchend", handleEnd],
+	["touchcancel", handleCancel],
+	["touchmove", handleMove]
+];
+
 const ongoingTouches = [];
 
 let penColor = "black";
@@ -16,53 +23,46 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function activateUIHandlers() {  
-  document.querySelector("#penSizeSlider").addEventListener('input', (event) => {
-    updatePenSize(event.target.value);
-  });
-  
-  document.querySelector("#flatSelect").addEventListener('input', () => {
-  	penMode = flat;
-  	penSize = 20;
-  });
-  
-  document.querySelector("#roundSelect").addEventListener('input', () => {
-  	penMode = round;
-  });
-  
-  document.querySelector("#fanSelect").addEventListener('input', () => {
-  	penMode = fan;
-  	penSize = 40;
-  });
-  
-  document.querySelector('#clear').addEventListener('input', () => {
+	document.querySelector("#penSizeSlider").addEventListener('input', (event) => {
+		updatePenSize(event.target.value);
+	});
+
+	document.querySelector("#flatSelect").addEventListener('input', () => {
+		penMode = flat;
+		penSize = 20;
+	});
+
+	document.querySelector("#roundSelect").addEventListener('input', () => {
+		penMode = round;
+	});
+
+	document.querySelector("#fanSelect").addEventListener('input', () => {
+		penMode = fan;
+		penSize = 40;
+	});
+
+	document.querySelector('#clear').addEventListener('input', () => {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-  });
+	});
 }
 
 // currently this clears the canvas during resize
 // need to save current/override clear
 function updateCanvasSize() {  
-  canvas.setAttribute('width', vvp.width);
-  canvas.setAttribute('height', vvp.height);
+	canvas.setAttribute('width', vvp.width);
+	canvas.setAttribute('height', vvp.height);
 }
 
 function activateViewportHandler() {
-  updateCanvasSize();
-  vvp.addEventListener("resize", updateCanvasSize);
+	updateCanvasSize();
+	vvp.addEventListener("resize", updateCanvasSize);
 }
 
 function activateTouchHandlers() {
-	const touchEventHandlers = [
-		["touchstart", handleStart],
-		["touchend", handleEnd],
-    ["touchcancel", handleCancel],
-    ["touchmove", handleMove]
-	];
-	
-	touchEventHandlers.forEach((eventHandler) => {
-		canvas.addEventListener(eventHandler[0], (event) => {
+	touchEventHandlers.forEach(([touchEvent, handler]) => {
+		canvas.addEventListener(touchEvent, (event) => {
 			event.preventDefault();
-			eventHandler[1](event);
+			handler(event);
 		});
 	});
 }
@@ -72,9 +72,9 @@ function handleStart(event) {
 
 	for (let i = 0; i < touches.length; i++) {
 		const touch = touches[i];
-		
+
 		if (penMode === round) { penMode(touch); }
-		
+
 		ongoingTouches.push(copyTouch(touch));
 	}
 }
@@ -86,12 +86,12 @@ function handleMove(event) {
 		const touchIndex = ongoingTouchIndexById(touches[i].identifier);
 
 		if (touchIndex !== -1) {
-		  const touch = touches[i];
-		  
-		  penMode(touch, touchIndex);
+			const touch = touches[i];
 
-		  // not splicing draws from origin to all moves,
-		  // potential feature!
+			penMode(touch, touchIndex);
+
+			// not splicing draws from origin to all moves,
+			// potential feature!
 			ongoingTouches.splice(touchIndex, 1, copyTouch(touch));
 		}
 	}
@@ -101,13 +101,13 @@ function handleEnd(event) {
 	const touches = event.changedTouches;
 
 	for (let i = 0; i < touches.length; i++) {
-	  const touch = touches[i];
+		const touch = touches[i];
 
 		const touchIndex = ongoingTouchIndexById(touch.identifier);
 
 		if (touchIndex !== -1) {
 			penMode(touch, touchIndex);
-			
+
 			ongoingTouches.splice(touchIndex, 1);
 		}
 	}
@@ -118,7 +118,7 @@ function handleCancel(event) {
 
 	for (let i = 0; i < touches.length; i++) {
 		const touchIndex = ongoingTouchIndexById(touches[i].identifier);
-		
+
 		ongoingTouches.splice(touchIndex, 1); // remove it; we're done
 	}
 }
@@ -134,11 +134,11 @@ function ongoingTouchIndexById(idToFind) {
 }
 
 function copyTouch({ identifier, pageX, pageY }) {
-  return { identifier, pageX, pageY };
+	return { identifier, pageX, pageY };
 }
 
 function clamp(value, min, max) {
-  return Math.min(Math.max(value, min), max)
+	return Math.min(Math.max(value, min), max)
 }
 
 function updatePenSize(val) {
@@ -149,11 +149,11 @@ function flat(touch, touchIndex) {
 	ctx.beginPath();
 	ctx.moveTo(ongoingTouches[touchIndex].pageX, ongoingTouches[touchIndex].pageY);
 	ctx.lineTo(touch.pageX, touch.pageY);
-	
+
 	ctx.lineWidth = (touch.force === 0) ? penSize : penSize * (1+touch.force);
 	ctx.strokeStyle = penColor;
 	ctx.stroke();
-	
+
 	// not splicing draws from origin to all moves,
 	// potential feature!
 	ongoingTouches.splice(touchIndex, 1, copyTouch(touch));
@@ -167,16 +167,16 @@ function fan(touch, touchIndex) {
 	ctx.lineWidth = penSize;
 	ctx.strokeStyle = penColor;
 	ctx.stroke();
-	
+
 	ongoingTouches.splice(touchIndex, 1, copyTouch(touch));
 }
 
 function round(touch) {
 	ctx.beginPath();
 	ctx.strokeStyle = penColor;
-	
+
 	ctx.arc(touch.pageX, touch.pageY, penSize, 0, 2 * Math.PI);
-	
+
 	ctx.fill();
 	ctx.stroke();
 }
